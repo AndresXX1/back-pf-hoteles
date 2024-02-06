5658b0088d0674183b84f961199e235c77d9b2ae
AndresveraXX <andres_9_09@hotmail.com>
2024-02-06 05:48:41 -0300

    dsa

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-// const { sequelize } = require("./src/db");
-// const server = require("./src/server");
+const { sequelize } = require("./src/db");
+const server = require("./src/server");
 
-// const startServer = async () => {
-//   await sequelize.sync({ force: false });
-//   server.listen(process.env.PORT || 3000, () => {
-//     console.log(`Server listening on port ${process.env.PORT || 3000}`);
-//   });
-// };
+const startServer = async () => {
+  await sequelize.sync({ force: false });
+  server.listen(process.env.PORT || 3000, () => {
+    console.log(`Server listening on port ${process.env.PORT || 3000}`);
+  });
+};
 
-// startServer();
-
-const express = require ("express");
-const app = express();
-
-const port = process.env.PORT || 3000;
-
-app.listen(port);
-
-console.log (`Listing on port ${port}`)
+startServer();
