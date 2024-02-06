7f7f0cc9b0a4b0672f99cfb5f30388d7d35556ee
AndresveraXX <andres_9_09@hotmail.com>
2024-02-06 04:47:35 -0300

    versel deploy

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-require("dotenv").config();
 const { sequelize } = require("./src/db");
 const server = require("./src/server");
 
-sequelize.sync({ force: false }).then(
+const startServer = async () => {
+  await sequelize.sync({ force: false });
   server.listen(process.env.PORT || 3000, () => {
-    console.log(`Server listening on port ${process.env.PORT || 3000 }`);
-  })
-);
+    console.log(`Server listening on port ${process.env.PORT || 3000}`);
+  });
+};
+
+startServer();
