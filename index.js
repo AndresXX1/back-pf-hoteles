15a99407ca4373ea8fdc4722bc3fa310ddb03ce9
AndresveraXX <andres_9_09@hotmail.com>
2024-02-06 05:55:13 -0300

    dsad

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
-// const { sequelize } = require("./src/db");
-// const server = require("./src/server");
+const loadDatabase = require("./loadDatabase");
+const { sequelize } = require("./src/db");
+const { Product } = require("./src/db");
+const server = require("./src/server");
 
-// const startServer = async () => {
-//   await sequelize.sync({ force: false });
-//   server.listen(process.env.PORT || 3000, () => {
-//     console.log(`Server listening on port ${process.env.PORT || 3000}`);
-//   });
-// };
+const PORT = 3000;
 
-// startServer();
-
-const express = require('express');
-const { sequelize } = require('./src/db');
-const serverRoutes = require('./src/server');
-
-const app = express();
-
-// Aquí puedes agregar cualquier middleware que necesites
-// app.use(...);
-
-// Agrega tus rutas al app
-app.use('/', serverRoutes);
-
-// Exporta una función que maneje las solicitudes HTTP
-module.exports = async (req, res) => {
-  // Inicializa la base de datos si es necesario
-  await sequelize.sync({ force: false });
-
-  // Pasa la solicitud y la respuesta a tu aplicación Express
-  return app(req, res);
-};
+sequelize
+  .sync({ force: true })
+  .then(async () => {
+    const allSnikers = await Product.findAll();
+    if (!allSnikers.length) {
+      loadDatabase();
+    } else {
+      console.log("Database Loaded");
+    }
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
