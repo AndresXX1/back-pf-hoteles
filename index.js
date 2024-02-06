5e92b2359e9e6f1e09117b04a480d472e2e619c8
AndresveraXX <andres_9_09@hotmail.com>
2024-02-06 05:31:47 -0300

    dasd

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const { sequelize } = require('./src/db');
-const serverRoutes = require('./src/server');
+// const { sequelize } = require("./src/db");
+// const server = require("./src/server");
 
+// const startServer = async () => {
+//   await sequelize.sync({ force: false });
+//   server.listen(process.env.PORT || 3000, () => {
+//     console.log(`Server listening on port ${process.env.PORT || 3000}`);
+//   });
+// };
+
+// startServer();
+
+const express = require ("express");
 const app = express();
 
-// Aquí puedes agregar cualquier middleware que necesites
-// app.use(...);
+const port = process.env.PORT || 3000;
 
-// Agrega tus rutas al app
-app.use('/', serverRoutes);
+app.listen(port);
 
-// Exporta una función que maneje las solicitudes HTTP
-module.exports = async (req, res) => {
-  // Inicializa la base de datos si es necesario
-  await sequelize.sync({ force: false });
-
-  // Pasa la solicitud y la respuesta a tu aplicación Express
-  return app(req, res);
-};
+console.log (`Listing on port ${port}`)
