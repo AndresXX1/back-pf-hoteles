bdb7de053e37c9c5ec8b0e11da684a5ceab78531
LeandroPom <leotrabajo02@gmail.com>
2024-02-08 19:46:28 -0300

    getProductByName, productByNameHandler, productsModel

--- src/controllers/products/getProductByName.js
+++ src/controllers/products/getProductByName.js
@@ -2,34 +2,26 @@ const { Product } = require("../../db");
 const { Sequelize } = require("sequelize");
 
 const getProductByName = async (name) => {
-const productByName = await Product.findAll({
-  where: {
-    [Sequelize.Op.or]: [
-      {
-        name: {
-          [Sequelize.Op.iLike]: `%${name}%`,
-        },
+  const productByName = await Product.findAll({
+    where: {
+      name: {
+        [Sequelize.Op.iLike]: `%${name}%`,
       },
-      Sequelize.literal(`"Product"."brand"::text ILIKE '%${name}%'`),
-      Sequelize.literal(`"Product"."colors"::text ILIKE '%${name}%'`)
-  
-    ],
-  },
-});
+    },
+  });
 
+  const results = productByName.map(product => ({
+      id: product.id,
+      nombre: product.name,
+      location: product.location,
+      season: product.season,
+      totalRooms: product.totalRooms,
+      pool: product.pool,
+      pricePerNight: `${product.pricePerNight} ars$ the night per person`,
+      image: product.image,
+  }));
 
-    const results = productByName.map(sneaker => ({
-        id: sneaker.id,
-        name: sneaker.name,
-        size: sneaker.size,
-        brand: sneaker.brand,
-        price: sneaker.price,
-        colors: Array.isArray(sneaker.colors) ? sneaker.colors : [],
-        image: sneaker.image
-    }))
-    
-    return results; 
-
+  return results;
 };
 
 module.exports = getProductByName;
--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define(
-    "Offer",
+    "Product",
     {
       id: {
         type: DataTypes.INTEGER,
