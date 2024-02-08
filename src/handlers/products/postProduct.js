b13dd3b6b8031b1022bb9f4cfd7034be7d84542e
MQuirico <128657405+MQuirico@users.noreply.github.com>
2024-02-08 12:11:55 -0300

    modelo productos y post products controller

--- src/handlers/products/postProduct.js
+++ src/handlers/products/postProduct.js
@@ -1,9 +1,9 @@
 const { postProduct } = require('../../controllers/products/postProductsDb');
 
 const postProductsHandler = async (req, res) => {
-  const { name, size,brand, price, colors ,image} = req.body;
+  const { name, location ,season, pricePerNight, totalRooms, pool ,image} = req.body;
   try {
-    const response = await postProduct(name, size, brand, price, colors,req,image);
+    const response = await postProduct(name, location, season, pricePerNight, totalRooms, pool, req, image);
     console.log(response);
     res.status(201).json(response);
   } catch (error) {
