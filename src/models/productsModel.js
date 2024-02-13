0dc8a7132432460dc93ca1b37c6b14e31ce394ff
AndresveraXX <andres_9_09@hotmail.com>
2024-02-13 02:57:23 -0300

    13/02 2:57 am

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -3,48 +3,49 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define(
     "Product",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        
+        name: {
+          type: DataTypes.STRING,
+          unique: true,
+          allowNull: false,
+        },
+  
+        location: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+  
+        pricePerNight: {
+          type: DataTypes.DECIMAL(10, 2),
+          allowNull: false,
+        },
+  
+        season: {
+          type: DataTypes.ARRAY(DataTypes.STRING),
+          allowNull: false,
+        },
+  
+        totalRooms: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        
+        pool: {
+          type: DataTypes.BOOLEAN, 
+          allowNull: false,
+        },
+        
+        images: {
+          type: DataTypes.ARRAY(DataTypes.STRING),
+          allowNull: true,
+        }
       },
-      name: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-      },
-      location: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        indexes: [
-          {
-            fields: ["location", "season"],
-          },
-        ],
-      },
-      pricePerNight: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
-      },
-      season: {
-        type: DataTypes.ARRAY(DataTypes.STRING),
-        allowNull: false,
-      },
-      totalRooms: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      pool: {
-        type: DataTypes.BOOLEAN, 
-        allowNull: false,
-      },
-      images: {
-        type: DataTypes.ARRAY(DataTypes.STRING),
-        allowNull: true,
-      
-      },
-    },
-    { timestamps: false, freezeTableName: true }
+      { timestamps: false, freezeTableName: true }
   );
 };
