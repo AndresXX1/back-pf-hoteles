5993de574d774f5daea19333605f31e12c70a2d0
MQuirico <128657405+MQuirico@users.noreply.github.com>
2024-02-27 21:51:58 -0300

    traer reservas para calendario

--- src/routes/recervasRouter.js
+++ src/routes/recervasRouter.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const {createReservation} = require("../controllers/recerba/postRecerva");
 const handlerProguctsReservas = require("../controllers/recerba/getProductsReservasHandler");
 const reservasByUser = require("../controllers/recerba/getReservasByUser")
+const handlerProductsIDReservas = require("../handlers/reserva/getReservabyProd")
 
 const RecerbaRouter = Router();
 
 RecerbaRouter.post("/get", handlerProguctsReservas);
 RecerbaRouter.get("/getByUserID/:userID", reservasByUser);
 RecerbaRouter.post("/new", createReservation);
+RecerbaRouter.get("/reservByProduct/:prodID",handlerProductsIDReservas)
 
 module.exports = RecerbaRouter;
